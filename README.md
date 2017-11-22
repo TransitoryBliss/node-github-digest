85bf2aa0545acad8af9f6fb3f9358e6a3750b96f
Robert Stenbom <robert.stenbom@advinans.com>
2017-11-22 11:51:51 +0100

    fix: add sign method

--- README.md
+++ README.md
@@ -1,15 +1,24 @@
 ## Validate GitHub webhook signatures
 
-Minimalistic package to validate the integrity of incoming GitHub webhooks.
+Minimalistic package to validate the integrity of incoming GitHub webhooks with
+zero dependencies.
+
+### Usage
 
 ```js
+// initialize with the secret
 const secret = process.env.GITHUB_SECRET;
-const isValid = require('validate-github-signature')(secret);
-// ...
-if (!isValid(req)) {
+const { validate, sign } = require('github-digest')(secret);
+
+// Validating:
+if (!validate(req)) {
   res.status(400);
   return res.json({ message: 'Unauthorized' });
 }
+
+// You can also sign your own requests (useful for integration testing)
+const payload = { body: { hello: 'world' } };
+assert.ok(validate(sign(payload))); // => ok
 ```
 
 ### Tests
--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,7 @@
 const secret = 'qwerty';
 const crypto = require('crypto');
-const validate = require('../')(secret);
+const index = require('../');
+const githubDigest = index(secret);
 
 const payload = {
   body: {
@@ -8,22 +9,13 @@ const payload = {
   },
 };
 
-const sign = (req, token) => {
-  const digest = crypto
-    .createHmac('sha1', token)
-    .update(JSON.stringify(req.body))
-    .digest('hex');
-
-  req.headers = req.headers || {};
-  req.headers['x-hub-signature'] = `sha1=${digest}`;
-  return req;
-};
-
 it('should return true when valid digest', () =>
-  expect(validate(sign(payload, secret))).toBe(true));
+  expect(githubDigest.validate(githubDigest.sign(payload))).toBe(true));
 
-it('should return false when invalid digest', () =>
-  expect(validate(sign(payload, 'dvorak'))).toBe(false));
+it('should return false when invalid digest', () => {
+  const { sign } = index('dvorak');
+  expect(githubDigest.validate(sign(payload))).toBe(false);
+});
 
 it('should return false when header is missing', () =>
-  expect(validate(payload)).toBe(false));
+  expect(githubDigest.validate(payload)).toBe(false));

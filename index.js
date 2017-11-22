7655c989d528355db99b6e7ff6026aacb73bebeb
Robert Stenbom <robert.stenbom@advinans.com>
2017-11-22 11:55:22 +0100

    fix: run prettier

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 const crypto = require('crypto');
 
-module.exports = secret => {
-  return {
-    validate(req) {
-      const signature = req.headers['x-hub-signature'];
-      if (!signature) return false;
+module.exports = secret => ({
+  validate(req) {
+    const signature = req.headers['x-hub-signature'];
+    if (!signature) return false;
 
-      const digest = crypto
-        .createHmac('sha1', secret)
-        .update(JSON.stringify(req.body))
-        .digest('hex');
+    const digest = crypto
+      .createHmac('sha1', secret)
+      .update(JSON.stringify(req.body))
+      .digest('hex');
 
-      return signature === `sha1=${digest}`;
-    },
-    sign(req) {
-      const digest = crypto
-        .createHmac('sha1', secret)
-        .update(JSON.stringify(req.body))
-        .digest('hex');
+    return signature === `sha1=${digest}`;
+  },
+  sign(req) {
+    const digest = crypto
+      .createHmac('sha1', secret)
+      .update(JSON.stringify(req.body))
+      .digest('hex');
 
-      req.headers = req.headers || {};
-      req.headers['x-hub-signature'] = `sha1=${digest}`;
-      return req;
-    },
-  };
-};
+    req.headers = req.headers || {};
+    req.headers['x-hub-signature'] = `sha1=${digest}`;
+    return req;
+  },
+});
